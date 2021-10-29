e380035735979c56504d47e46d3951a7b30869e7
betonucon <betonucon@gmail.com>
2021-10-29 16:40:44 +0700

    perbaikan

--- resources/views/layouts/app_web.blade.php
+++ resources/views/layouts/app_web.blade.php
@@ -286,7 +286,7 @@
 		
         <!-- begin theme-panel -->
         <div class="theme-panel">
-            <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
+            <!-- <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a> -->
             <div class="theme-panel-content">
                 <h5 class="m-t-0">Color Theme</h5>
                 <ul class="theme-list clearfix">
